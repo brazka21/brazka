46fe2c8f1d6a0a6e08de3b364ec5442718c31122
brazka21 <razimcox@gmail.com>
2026-09-21 15:50:20 +1000

    Add customer review highlights

--- scripts/build-pages.py
+++ scripts/build-pages.py
@@ -23,7 +23,7 @@ base=base.replace('Для каталога включи JavaScript или','Чт
 base=base.replace('href="./"','href="/"')
 base=re.sub(r'(href|src)="(assets/|storefront\.)',r'\1="/\2',base)
 base=re.sub(r'storefront\.js\?v=[^"\']+','storefront.js?v=conversion3',base)
-base=re.sub(r'storefront\.css\?v=[^"\']+','storefront.css?v=conversion3',base)
+base=re.sub(r'storefront\.css\?v=[^"\']+','storefront.css?v=reviews1',base)
 if 'mc.yandex.ru/metrika/tag.js?id=112697107' not in base:
  base=base.replace('</head>', '''<!-- Yandex.Metrika counter -->
 <script>
